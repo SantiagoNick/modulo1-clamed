953706118a0b65b2e908125e1c83903771a53711
SantiagoNick <nicksantiago19@gmail.com>
2022-07-07 18:52:13 -0300

    Alterações na Semana 2

--- Módulo 1/Semana 2/script/index.js
+++ Módulo 1/Semana 2/script/index.js
@@ -3,3 +3,12 @@ var sobrenome = window.prompt("Seu sobrenome");
 
 console.log(nome + " " + sobrenome);
 document.write(nome + " " + sobrenome);
+
+var nota = window.prompt("Sua nota");
+if (nota >= 7) {
+  console.log(
+    "Parabéns, você tirou a média mínima. Não esqueça de continuar estudando ;)"
+  );
+} else {
+  console.log("Você não foi aprovado");
+}
